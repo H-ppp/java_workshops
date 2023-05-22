2179b6486f79ced327cbbae25b0d8bd426b500b2
H-ppp <128716661+H-ppp@users.noreply.github.com>
2023-05-22 09:58:26 +0300

    Add files via upload

---
+++ lr1/src/main/java/com/example/lr1/logicofcomputing/FindMinMaxAver.java
@@ -0,0 +1,44 @@
+package com.example.lr1.logicofcomputing;
+
+
+import java.util.ArrayList;
+
+import org.springframework.boot.configurationprocessor.json.JSONException;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+public class FindMinMaxAver {
+    
+    public FindMinMaxAver()
+    {
+    }
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public JSONObject listToAggJson(ArrayList<Integer> al) {
+
+        
+        LOGGER.info("Generated values: " + al.toString());
+        int minVal = al.stream().min(Integer::compareTo).get();
+        LOGGER.info("Min value: " + minVal);
+
+        int maxVal = al.stream().max(Integer::compareTo).get();
+        LOGGER.info("Max value: " + maxVal);
+
+        Double averVal = al.stream().mapToInt(n -> n).average().orElseThrow( IllegalArgumentException :: new);
+        LOGGER.info("Average value: " + averVal);
+
+        
+        JSONObject resp = new JSONObject();
+        try {
+            resp.put("min value: ", minVal);
+            resp.put("max value: ", maxVal);
+            resp.put("average value: ", averVal);
+            resp.put("generated values: ", al.toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return resp;
+    }
+}
---
+++ lr1/src/main/java/com/example/lr1/logicofcomputing/RandCalculate.java
@@ -0,0 +1,56 @@
+package com.example.lr1.logicofcomputing;
+
+import com.example.lr1.exception.IllegalArguments;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.ArrayList;
+
+import java.util.Collections;
+
+import java.util.stream.Collectors;
+
+public class RandCalculate {
+
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public RandCalculate() {
+    };
+
+    public ArrayList<Integer> generateNums(Integer num)
+            throws IllegalArgumentException, IllegalArguments, InterruptedException {
+
+        if (num < 5) {
+            LOGGER.info("Entered number shall not be less than 5. Your number: " + num);
+            throw new IllegalArguments("Entered number must not be less than 5");
+        }
+        if (num > 100) {
+            LOGGER.error("Entered number must not be greater than 100");
+            throw new IllegalArgumentException("Entered number must not be greater than 100");
+        }
+
+        ArrayList<Integer> randVals = new ArrayList<Integer>();
+        ArrayList<Integer> lambdaRandVals = new ArrayList<Integer>();
+        try {
+            Thread.sleep(15000);
+        } catch (InterruptedException e) {
+           
+            e.printStackTrace();
+        }
+        for (int i = 1; i <= 5; i++) {
+            int yourNumber = (int) (Math.random() * (num + 1));
+            randVals.add(yourNumber);
+
+        }
+
+        lambdaRandVals = randVals;
+        lambdaRandVals.stream().map(x -> x + 1);
+        Collections.sort(lambdaRandVals, (o1, o2) -> (o1 < o2) ? -1 : (o1 > o2) ? 1 : 0);
+        Collectors.toList();
+        randVals = lambdaRandVals;
+
+        return randVals;
+    }
+
+}
